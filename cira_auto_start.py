c7d68913e59de7168c310ad2e3186d3cab024b75
CiRA-AMI <30405116+CiRA-AMI@users.noreply.github.com>
2024-03-26 17:06:17 +0700

    Update cira_auto_start.py

--- cira_auto_start.py
+++ cira_auto_start.py
@@ -20,18 +20,30 @@ RUN_ARDUINO = False
 
 time.sleep(10)
 
-new_proc = Popen(["rosversion", "-d"], stdout=subprocess.PIPE, text=True)
-ros_version = str(new_proc.communicate()[0]).replace("\n", "")
-print('ROS : ', ros_version)
+def find_ros_version():
+    preferred_versions = ['noetic', 'melodic']
+    ros_base_path = '/opt/ros/'
+    for version in preferred_versions:
+        version_path = os.path.join(ros_base_path, version)
+        if os.path.isdir(version_path):
+            return version  # Return the first found version
+    return None
+
+ros_version = find_ros_version()
+if ros_version:
+    print(f'ROS version found: {ros_version}')
+else:
+    print('No preferred ROS version found in /opt/ros/')
+
 
 # 1 run ros core
-cmd = f"source /opt/ros/{ros_version}/setup.bash && roscore"
+cmd = f"source ~/.bashrc && source /opt/ros/{ros_version}/setup.bash && roscore"
 print("cmd : ", cmd)
 Popen(["gnome-terminal", '--' , 'bash', '-c' , cmd])
 time.sleep(3)
 
 # 3 run cira core
-cmd = "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && "
+cmd = "source ~/.bashrc && LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && "
 cmd = cmd + f"source /opt/ros/{ros_version}/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && "
 cmd = cmd + f"while true; do rosrun cira_core cira_core_run  _file:=\'{FLOW_FILE}\' _hide_toolbar:={HIDE_TOOLBAR} _lock:={LOCK_CIRA_CORE} _fullscreen:={SHOW_FULLSCREEN}; sleep 1s; done ;$SHELL"
 print("cmd : ", cmd)
@@ -42,8 +54,8 @@ time.sleep(2)
 
 if RUN_ARDUINO :
     # 2 run arduino
-    cmd = "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && "
+    cmd = "source ~/.bashrc && LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && "
     cmd = cmd + f"source /opt/ros/{ros_version}/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && while true; do rosrun cira_arduino_io cira_arduino_io_run _pass:={USER_PASS}; sleep 1s; done ;$SHELL"
     print("cmd : ", cmd)
-    Popen(["gnome-terminal", '--' , 'bash', '-c' , cmd])
+    Popen(["gnome-terminal", '--' , '/bin/bash', '-c' , cmd])
     time.sleep(2)
